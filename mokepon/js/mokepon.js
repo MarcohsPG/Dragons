a5a072bbedb9a3b27a106e5724b41509cbc4273d
MarcohsPG <marcospuertosgomez@gmail.com>
2022-08-10 15:03:21 -0500

    Colision de dos jugadores si ataques Funcionando

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -28,6 +28,11 @@ const ataquesEnemigo = document.getElementById('ataques-enemigo')
 
 //crearMensajeFinal (se repiten las variables)
 
+//Variables para capitulo 60 en adelante(CANVAS)
+const sectionVerMapa = document.getElementById('ver-mapa')
+const mapa = document.getElementById('mapa')
+const lienzo = mapa.getContext("2d")
+
 //Variables globales
 let ataqueJugador=[]
 let ataquesDragonEnemigo
@@ -36,6 +41,7 @@ let resultado
 let vidasJugador = 3
 let vidasEnemigo = 3
 let dragones = []
+let dragonesEnemigos = []
 let opcionDeDragones
 let idImgJugador
 let chimuelo 
@@ -50,47 +56,84 @@ let indexAtaqueJugador
 let indexAtaqueEnemigo
 let victorias=0
 let derrotas=0
+let intervalo
+let mapabackground= new Image()
+mapabackground.src='/mokepon/img/pueblo.jpg'
+let dragonJugadorObj
+let jugadorId = null
+let alturaQueBuscamos
+let anchoDelMapa = window.innerWidth -300
+let anchoMaximoMapa=350
+alturaQueBuscamos = anchoDelMapa*600/1600
+if(anchoMaximoMapa>anchoDelMapa){
+    anchoDelMapa=anchoMaximoMapa-80
+}
+mapa.width=anchoDelMapa
+mapa.height=alturaQueBuscamos
 
 //CLASES
 class Dragon{
-    constructor(nombre,img,vida){
+    constructor(nombre,img,vida,fotoMapa,id = null){
+        this.id = id
         this.nombre = nombre
         this.img = img
         this.vida = vida
         this.ataques = []
+        this.ancho = 120
+        this.alto = 120
+        this.x = aleatorio(0,mapa.width-this.ancho)
+        this.y = aleatorio(0,mapa.height-this.alto)
+        this.mapaFoto = new Image()
+        this.mapaFoto.src = fotoMapa
+        this.velocidadX=0
+        this.velocidadY=0
+    }
+
+    pintarDragon(){
+        lienzo.drawImage(
+            this.mapaFoto,
+            this.x,
+            this.y,
+            this.ancho,
+            this.alto
+        )
     }
 }
 //OBJETOS
-let objChimuelo= new Dragon('Chimuelo','./img/chimuelo.png',5)
-let objRompecraneos = new Dragon('Rompecraneos','./img/Vanidoso.png',5)
-let objRatigueya = new Dragon('Ratigueya', './img/ratigueya.png',5)
+let objChimuelo= new Dragon('Chimuelo','./img/chimuelo.png',5,'/mokepon/img/chimueloMapa.png')
+let objRompecraneos = new Dragon('Rompecraneos','./img/Vanidoso.png',5,'/mokepon/img/Rompecraneos.png')
+let objRatigueya = new Dragon('Ratigueya', './img/ratigueya.png',5,'/mokepon/img/ratigueyaMapa.png')
 
-//ARRAY DE OBJETOS
-
-objChimuelo.ataques.push(
+const CHIMUELO_ATAQUES=[
     {nombre:'💦', id:'btn-agua'},
     {nombre:'💦',id:'btn-agua'},
     {nombre:'💦',id:'btn-agua'},
     {nombre:'🏔️',id:'btn-tierra'},
     {nombre:'🔥',id:'btn-fuego'},
+]
 
-)
-objRompecraneos.ataques.push(
+objChimuelo.ataques.push(...CHIMUELO_ATAQUES)
+
+
+const ROMPECRANEOS_ATQUES=[
     {nombre:'🔥',id:'btn-fuego'},
     {nombre:'🔥',id:'btn-fuego'},
     {nombre:'🔥',id:'btn-fuego'},
     {nombre:'💦', id:'btn-agua'},
     {nombre:'🏔️',id:'btn-tierra'},
 
-)
-objRatigueya.ataques.push(
+]
+objRompecraneos.ataques.push(...ROMPECRANEOS_ATQUES)
+
+const RATIGUEYA_ATAQUES = [
     {nombre:'🏔️',id:'btn-tierra'},
     {nombre:'🏔️',id:'btn-tierra'},
     {nombre:'🏔️',id:'btn-tierra'},
     {nombre:'💦', id:'btn-agua'},
     {nombre:'🔥',id:'btn-fuego'},
+]
 
-)
+objRatigueya.ataques.push(...RATIGUEYA_ATAQUES)
 
 dragones.push(objChimuelo,objRompecraneos,objRatigueya)
 
@@ -98,6 +141,7 @@ function iniciarJuego(){
     
     sectionreiniciarJuego.style.display='none'
     sectionSeleccionarAtaque.style.display='none'
+    sectionVerMapa.style.display='none'
 
     dragones.forEach((dragon)=>{
         opcionDeDragones = `
@@ -114,10 +158,25 @@ function iniciarJuego(){
     btndragon.addEventListener('click', seleccdragon)
     
     btnReiniciar.addEventListener('click',reiniciarJuego)
+
+    //Back NodeJS
+    unirseAlJuego()
+}
+
+function unirseAlJuego(){
+    fetch("http://localhost:8080/unirse")
+        .then(function(res){
+            if(res.ok){
+                res.text()
+                .then(function (respuesta){
+                    console.log(respuesta)
+                    jugadorId = respuesta
+                })
+            }
+        })
 }
 function seleccdragon(){
     
-    sectionSeleccionarAtaque.style.display='flex'
     sectionSeleccionardragon.style.display='none'
 
     if(chimuelo.checked){
@@ -137,8 +196,28 @@ function seleccdragon(){
         reiniciarJuego()
     }
 
+    //Back NodeJS
+    seleccionarDragonBack(idImgJugador)
+
     extraerAtaques(idImgJugador)
-    seleccdragonEnemigo()
+
+    sectionVerMapa.style.display='flex'
+    //MAPA(CANVAS)
+    iniciarMapa()
+}
+
+function seleccionarDragonBack(idImgJugador){
+    fetch(`http://localhost:8080/dragon/${jugadorId}`,{
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nombre: idImgJugador
+        })
+})
+        
+
 }
 
 function extraerAtaques(idImgJugador){
@@ -212,11 +291,10 @@ function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-function seleccdragonEnemigo(){
-    let dragonEnemigoAleatorio = aleatorio(0,dragones.length-1)
-    spandragonEnemigo.innerHTML= dragones[dragonEnemigoAleatorio].nombre
-    imgDragonEnemigo.innerHTML= `<img src="${dragones[dragonEnemigoAleatorio].img}" />`
-    ataquesDragonEnemigo =dragones[dragonEnemigoAleatorio].ataques
+function seleccdragonEnemigo(enemigo){
+    spandragonEnemigo.innerHTML= enemigo.nombre
+    imgDragonEnemigo.innerHTML= `<img src="${enemigo.img}" />`
+    ataquesDragonEnemigo =enemigo.ataques
     secuenciaAtaque()
 }
 
@@ -308,4 +386,144 @@ function reiniciarJuego(){
     location.reload()
 }
 
+//CANVAS
+function pintarCanvas(){
+
+    dragonJugadorObj.x=dragonJugadorObj.x + dragonJugadorObj.velocidadX
+    dragonJugadorObj.y=dragonJugadorObj.y + dragonJugadorObj.velocidadY
+    lienzo.clearRect(0,0,mapa.width,mapa.height)
+    lienzo.drawImage(
+        mapabackground,
+        0,
+        0,
+        mapa.width,
+        mapa.height
+    )
+    dragonJugadorObj.pintarDragon()
+    enviarPosicion(dragonJugadorObj.x,dragonJugadorObj.y)
+
+    dragonesEnemigos.forEach(function(dragon){
+        dragon.pintarDragon()
+        revisarColision(dragon)
+    })
+    
+}
+
+function enviarPosicion(x,y){
+    fetch(`http://localhost:8080/dragon/${jugadorId}/posicion`,{
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            x,
+            y
+        })
+    })
+    .then(function (res){
+        if(res.ok){
+            res.json()
+                .then(function({enemigos}){
+                    console.log(enemigos)
+                    dragonesEnemigos = enemigos.map(function(enemigo){
+                        let dragonEnemigo = null
+                        const dragonNombre  = enemigo.dragon.nombre || ""
+                        if(dragonNombre === "Chimuelo"){
+                            dragonEnemigo= new Dragon('Chimuelo','./img/chimuelo.png',5,'/mokepon/img/chimueloMapa.png')
+                        }else if (dragonNombre === "Rompecraneos"){
+                            dragonEnemigo = new Dragon('Rompecraneos','./img/Vanidoso.png',5,'/mokepon/img/Rompecraneos.png')
+                        }else if(dragonNombre === "Ratigueya"){
+                            dragonEnemigo = new Dragon('Ratigueya', './img/ratigueya.png',5,'/mokepon/img/ratigueyaMapa.png')
+                        }
+
+                        dragonEnemigo.x = enemigo.x
+                        dragonEnemigo.y = enemigo.y
+                        return dragonEnemigo
+
+                    })
+                })
+        }
+    })
+}
+
+
+
+
+function moverDerecha(){
+    dragonJugadorObj.velocidadX=5
+}
+function moverIzquierda(){
+    dragonJugadorObj.velocidadX=-5
+}
+function moverAbajo(){
+    dragonJugadorObj.velocidadY=5
+}
+function moverArriba(){
+    dragonJugadorObj.velocidadY=-5
+}
+
+function detenerMovimiento(){
+    dragonJugadorObj.velocidadX=0
+    dragonJugadorObj.velocidadY=0
+}
+function sePresionoUnaTecla(event){
+    switch (event.key) {
+        case 'ArrowUp':
+            moverArriba()
+            break;
+        case 'ArrowDown':
+            moverAbajo()
+            break;  
+        case 'ArrowRight':
+            moverDerecha()
+            break;  
+        case 'ArrowLeft':
+            moverIzquierda()
+            break;
+        default:
+            break;
+    }
+}
+
+function iniciarMapa(){
+    //CANVAS
+    dragonJugadorObj = obtenerObjetoDragon(idImgJugador)
+    intervalo=setInterval(pintarCanvas,50)
+
+    //TECLAS
+    window.addEventListener('keydown', sePresionoUnaTecla)
+    window.addEventListener('keyup',detenerMovimiento)
+}
+
+function obtenerObjetoDragon(){
+    for (let i = 0; i < dragones.length; i++) {
+        if(idImgJugador==dragones[i].nombre){
+            return dragones[i]
+        }        
+    }
+}
+function revisarColision(enemigo){
+    const arribaEnemigo = enemigo.y
+    const abajoEnemigo = enemigo.y + enemigo.alto
+    const derechaEnemigo = enemigo.x + enemigo.ancho
+    const izquierdaEnemigo = enemigo.x
+    const arribaDragon = dragonJugadorObj.y
+    const abajoDragon = dragonJugadorObj.y + dragonJugadorObj.alto
+    const derechaDragon = dragonJugadorObj.x + dragonJugadorObj.ancho
+    const izquierdaDragon = dragonJugadorObj.x
+    if(
+        abajoDragon < arribaEnemigo ||
+        arribaDragon > abajoEnemigo ||
+        derechaDragon < izquierdaEnemigo ||
+        izquierdaDragon > derechaEnemigo
+    ){
+        return
+    }
+    detenerMovimiento()
+    clearInterval(intervalo )
+    sectionSeleccionarAtaque.style.display='flex'
+    sectionVerMapa.style.display='none'
+    //alert("Hay colisión " + enemigo.nombre)
+    seleccdragonEnemigo(enemigo)
+}
 window.addEventListener('load', iniciarJuego)
